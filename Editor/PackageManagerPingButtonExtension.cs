08b39c2871bfc5333807d8dcadaa3f79dc80a6c0
baba-s <shotababa1206@gmail.com>
2022-08-26 19:47:50 +0900

    first commit

---
+++ Editor/PackageManagerPingButtonExtension.cs
@@ -0,0 +1,74 @@
+﻿using UnityEditor;
+using UnityEditor.PackageManager.UI;
+using UnityEngine.UIElements;
+using PackageInfo = UnityEditor.PackageManager.PackageInfo;
+
+namespace Kogane.Internal
+{
+    [InitializeOnLoad]
+    internal sealed class PackageManagerPingButtonExtension
+        : VisualElement,
+          IPackageManagerExtension
+    {
+        private bool        m_isInitialized;
+        private PackageInfo m_selectedPackageInfo;
+
+        static PackageManagerPingButtonExtension()
+        {
+            var extension = new PackageManagerPingButtonExtension();
+            PackageManagerExtensions.RegisterExtension( extension );
+        }
+
+        VisualElement IPackageManagerExtension.CreateExtensionUI()
+        {
+            m_isInitialized = false;
+            return this;
+        }
+
+        void IPackageManagerExtension.OnPackageSelectionChange( PackageInfo packageInfo )
+        {
+            Initialize();
+
+            m_selectedPackageInfo = packageInfo;
+        }
+
+        private void Initialize()
+        {
+            if ( m_isInitialized ) return;
+
+            VisualElement root = this;
+
+            while ( root is { parent: { } } )
+            {
+                root = root.parent;
+            }
+
+            var pingButton = new Button
+            (
+                () =>
+                {
+                    var assetPath = m_selectedPackageInfo.assetPath;
+                    var asset     = AssetDatabase.LoadAssetAtPath<UnityEngine.Object>( assetPath );
+
+                    EditorGUIUtility.PingObject( asset );
+                }
+            )
+            {
+                text = "Ping",
+            };
+
+            var removeButton = root.FindElement( x => x.name == "PackageRemoveCustomButton" );
+            removeButton.parent.Insert( 0, pingButton );
+
+            m_isInitialized = true;
+        }
+
+        void IPackageManagerExtension.OnPackageAddedOrUpdated( PackageInfo packageInfo )
+        {
+        }
+
+        void IPackageManagerExtension.OnPackageRemoved( PackageInfo packageInfo )
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Kogane Package Manager Ping Button Extension
+
+Project ウィンドウにおけるパッケージの場所を強調表示する Ping ボタンを Package Manager に追加するエディタ拡張
